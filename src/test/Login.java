b6a46357d76ecaab7c3a4d8b308f7797d5f9ea40
hamed2019 <hamedhashemi91@yahoo.com>
2020-02-29 12:34:31 -0500

    udating login from intelij at 12:34 pm

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,7 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Hello from Git project");
+
+        System.out.println("this is a new update in this class at 12:30 pm");
     }
 }
