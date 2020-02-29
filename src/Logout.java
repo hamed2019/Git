99d5a1f5f2fde23a7f23f1d89d47ec265c531f35
hamed2019 <hamedhashemi91@yahoo.com>
2020-02-29 11:33:49 -0500

    update logout.java

--- src/Logout.java
+++ src/Logout.java
@@ -2,5 +2,9 @@ public class Logout {
 
     public static void main(String[] args) {
         System.out.println("this is second class in git project caleed \"Logout\"");
+
+        //hi
+
     }
+
 }
